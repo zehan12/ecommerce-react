9ed32a8da92a3b4417de4e474cd9e140a25793e1
zehan12 <zehan9211@gmail.com>
2024-11-28 02:50:56 +0530

    add name

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
           <span>
             Developed by
             {' '}
-            <a href="https://github.com/jgudo">JULIUS GUEVARRA</a>
+            <a href="https://github.com/zehan12">zehan khan/a>
           </span>
         </strong>
       </div>
@@ -33,7 +33,7 @@ const Footer = () => {
         <strong>
           <span>
             Fork this project &nbsp;
-            <a href="https://github.com/jgudo/ecommerce-react">HERE</a>
+            <a href="https://github.com/zehan12/ecommerce-react">HERE</a>
           </span>
         </strong>
       </div>
